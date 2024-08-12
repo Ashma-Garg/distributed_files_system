f536d715d9722e1042e084c9597bc08445316a99
Rugvedi More <morer@delta.cs.uwindsor.ca>
2024-08-12 05:42:47 -0400

    dtar only

---
+++ Smain.c
@@ -0,0 +1,502 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define PORT 8090
+#define BUFFER_SIZE 1024
+#define SPDF_PORT 8093
+#define STEXT_PORT 8092
+#define TEXT_ADDRESS "127.0.0.2"
+#define PDF_ADDRESS "127.0.0.3"
+
+void handle_client(int client_sock);
+void handle_ufile(int client_sock, char *filename, char *dest_path, char *buffer);
+void handle_dfile(int client_sock, char *filename, char *buffer);
+void handle_rmfile(int client_sock, char *filename);
+void handle_dtar(int client_sock, char *filetype);
+void handle_display(int client_sock, char *pathname);
+void connect_to_server(const char *ip, int port, int *sock);
+
+int main()
+{
+    int server_sock, client_sock;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+
+    if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY; // 127.0.0.1
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Bind failed");
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_sock, 3) < 0)
+    {
+        perror("Listen failed");
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Smain server listening on port %d...\n", PORT);
+
+    while ((client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &addr_len)) >= 0)
+    {
+        printf("New client connected to Smain\n");
+
+        if (fork() == 0)
+        {
+            close(server_sock);
+            handle_client(client_sock);
+            close(client_sock);
+            exit(0);
+        }
+        else
+        {
+            close(client_sock);
+        }
+    }
+
+    if (client_sock < 0)
+    {
+        perror("Accept failed");
+        close(server_sock);
+        exit(EXIT_FAILURE);
+    }
+
+    close(server_sock);
+    return 0;
+}
+
+void handle_client(int client_sock)
+{
+    char buffer[BUFFER_SIZE];
+    char command[BUFFER_SIZE];
+    char arg1[BUFFER_SIZE], arg2[BUFFER_SIZE];
+
+    while (1)
+    {
+        memset(buffer, 0, BUFFER_SIZE);
+        memset(command, 0, BUFFER_SIZE);
+        memset(arg1, 0, BUFFER_SIZE);
+        memset(arg2, 0, BUFFER_SIZE);
+
+        // Receive data from client
+        int recv_len = recv(client_sock, buffer, BUFFER_SIZE - 1, 0);
+        if (recv_len <= 0)
+        {
+            if (recv_len == 0)
+            {
+                printf("Client disconnected\n");
+            }
+            else
+            {
+                perror("recv failed");
+            }
+            break;
+        }
+
+        // Null-terminate the buffer
+        buffer[recv_len] = '\0';
+
+        // Parse command
+        sscanf(buffer, "%s %s %s", command, arg1, arg2);
+
+        printf("Received command: %s\n", command);
+        printf("arg1: %s\n", arg1);
+        printf("arg2: %s\n", arg2);
+
+        // Handle command
+        if (strcmp(command, "ufile") == 0)
+        {
+            handle_ufile(client_sock, arg1, arg2, buffer);
+        }
+        else if (strcmp(command, "dfile") == 0)
+        {
+            handle_dfile(client_sock, arg1, buffer);
+        }
+        else if (strcmp(command, "rmfile") == 0)
+        {
+            handle_rmfile(client_sock, arg1);
+        }
+        else if (strcmp(command, "dtar") == 0)
+        {
+            handle_dtar(client_sock, arg1);
+        }
+        else if (strcmp(command, "display") == 0)
+        {
+            handle_display(client_sock, arg1);
+        }
+        else
+        {
+            char *msg = "Invalid command\n";
+            send(client_sock, msg, strlen(msg), 0);
+        }
+    }
+}
+
+int create_directory_recursive(const char *path)
+{
+    char temp[256];
+    char *p = NULL;
+    size_t len;
+
+    snprintf(temp, sizeof(temp), "%s", path);
+    len = strlen(temp);
+
+    if (temp[len - 1] == '/')
+    {
+        temp[len - 1] = '\0';
+    }
+
+    for (p = temp + 1; *p; p++)
+    {
+        if (*p == '/')
+        {
+            *p = '\0';
+            if (mkdir(temp, 0755) != 0 && errno != EEXIST)
+            {
+                perror("mkdir");
+                return -1;
+            }
+            *p = '/';
+        }
+    }
+
+    if (mkdir(temp, 0755) != 0 && errno != EEXIST)
+    {
+        perror("mkdir");
+        return -1;
+    }
+
+    return 0;
+}
+
+void handle_ufile(int client_sock, char *filename, char *dest_path, char *buffer)
+{
+    char file_path[BUFFER_SIZE];
+    char destination_path[BUFFER_SIZE];
+    FILE *file;
+    char response[BUFFER_SIZE];
+    char file_buffer[BUFFER_SIZE];
+    int bytes_received;
+
+    if (strstr(filename, ".c") != NULL)
+    {
+        // Construct the file path on the server
+        snprintf(destination_path, sizeof(destination_path), "/home/garg83/smain/%s", dest_path);
+        // Create the destination directory if it doesn't exist
+        if (create_directory_recursive(destination_path) != 0)
+        {
+            return;
+        }
+
+        snprintf(file_path, sizeof(file_path), "/home/garg83/smain/%s/%s", dest_path, filename);
+        // Open the file for writing
+        file = fopen(file_path, "wb");
+        if (file == NULL)
+        {
+            snprintf(response, sizeof(response), "Failed to open file %s for writing\n", file_path);
+            send(client_sock, response, strlen(response), 0);
+            return;
+        }
+
+        // Receive file data from the client
+        printf("Receiving file: %s\n", file_path);
+        while ((bytes_received = recv(client_sock, file_buffer, sizeof(file_buffer), 0)) > 0)
+        {
+            printf("Received %d bytes\n", bytes_received);
+            fwrite(file_buffer, 1, bytes_received, file);
+            if (bytes_received < sizeof(file_buffer))
+            {
+                printf("End of file detected\n");
+                break; // End of file
+            }
+        }
+
+        snprintf(response, sizeof(response), "File %s uploaded successfully\n", filename);
+        send(client_sock, response, strlen(response), 0);
+    }
+    else if (strstr(filename, ".txt") != NULL)
+    {
+        int stext_sock;
+        connect_to_server(TEXT_ADDRESS, STEXT_PORT, &stext_sock);
+        if (send(stext_sock, buffer, BUFFER_SIZE, 0) == -1)
+        {
+            snprintf(response, sizeof(response), "Not able to connect socket for text files", filename);
+            send(client_sock, response, strlen(response), 0);
+        }
+
+        while ((bytes_received = recv(client_sock, file_buffer, sizeof(file_buffer), 0)) > 0)
+        {
+            send(stext_sock, file_buffer, bytes_received, 0);
+            if (bytes_received < sizeof(file_buffer))
+            {
+                break; // End of file
+            }
+        }
+        int len = recv(stext_sock, response, sizeof(response) - 1, 0);
+        send(client_sock, response, len, 0);
+        close(stext_sock);
+    }
+    else if (strstr(filename, ".pdf") != NULL)
+    {
+        int spdf_sock;
+
+        connect_to_server(PDF_ADDRESS, SPDF_PORT, &spdf_sock);
+        if (send(spdf_sock, buffer, BUFFER_SIZE, 0) == -1)
+        {
+            snprintf(response, sizeof(response), "Not able to connect socket for pdf files", filename);
+            send(client_sock, response, strlen(response), 0);
+        }
+
+        while ((bytes_received = recv(client_sock, file_buffer, sizeof(file_buffer), 0)) > 0)
+        {
+            send(spdf_sock, file_buffer, bytes_received, 0);
+            if (bytes_received < sizeof(file_buffer))
+            {
+                break; // End of file
+            }
+        }
+
+        int len = recv(spdf_sock, response, sizeof(response) - 1, 0);
+        send(client_sock, response, len, 0);
+        close(spdf_sock);
+    }
+    else
+    {
+        snprintf(response, sizeof(response), "File %s not supported for this process.\n", filename);
+        send(client_sock, response, strlen(response), 0);
+    }
+}
+
+void handle_dfile(int client_sock, char *filename, char *initial_command)
+{
+    char buffer[BUFFER_SIZE];
+    char file_path[BUFFER_SIZE];
+    int file;
+    int file_size;
+    char response[BUFFER_SIZE];
+
+    if (strstr(filename, ".c") != NULL)
+    {
+        snprintf(file_path, sizeof(file_path), "/home/garg83/smain/%s", filename);
+        printf("File: %s\n", file_path);
+
+        file = open(file_path, O_RDONLY);
+
+        if (file < 0)
+        {
+            perror("Failed to open file");
+            return;
+        }
+
+        while ((file_size = read(file, buffer, sizeof(buffer))) >= 0)
+        {
+            if (file_size == 0)
+            {
+                send(client_sock, buffer, 1, 0);
+            }
+            else
+            {
+                send(client_sock, buffer, file_size, 0);
+            }
+            if (file_size < sizeof(buffer))
+            {
+                break; // End of file
+            }
+        }
+
+        close(file);
+        sleep(5);
+        snprintf(response, sizeof(response), "File %s uploaded successfully\n", filename);
+        send(client_sock, response, strlen(response), 0);
+    }
+    // If file is .txt or .pdf, fetch from Spdf or Stext server
+    else if (strstr(filename, ".txt") != NULL)
+    {
+        int stext_sock;
+        connect_to_server(TEXT_ADDRESS, STEXT_PORT, &stext_sock);
+        send(stext_sock, initial_command, strlen(initial_command), 0);
+        while ((file_size = recv(stext_sock, buffer, BUFFER_SIZE, 0)) >= 0)
+        {
+            send(client_sock, buffer, file_size, 0);
+            if (file_size < sizeof(buffer))
+            {
+                break; // End of file
+            }
+        }
+        int len = recv(stext_sock, response, sizeof(response), 0);
+        send(client_sock, response, strlen(response), 0);
+        close(stext_sock);
+    }
+    else if (strstr(filename, ".pdf") != NULL)
+    {
+        int spdf_sock;
+        connect_to_server(PDF_ADDRESS, SPDF_PORT, &spdf_sock);
+        send(spdf_sock, initial_command, strlen(initial_command), 0);
+        while ((file_size = recv(spdf_sock, buffer, BUFFER_SIZE, 0)) >= 0)
+        {
+            send(client_sock, buffer, file_size, 0);
+            if (file_size < sizeof(buffer))
+            {
+                break; // End of file
+            }
+        }
+
+        int len = recv(spdf_sock, response, sizeof(response), 0);
+        send(client_sock, response, sizeof(response), 0);
+        close(spdf_sock);
+    }
+    else
+    {
+        snprintf(response, sizeof(response), "File %s not supported for this process.\n", filename);
+        send(client_sock, response, strlen(response), 0);
+    }
+}
+
+void handle_rmfile(int client_sock, char *filename)
+{
+    char file_path[BUFFER_SIZE];
+
+    snprintf(file_path, sizeof(file_path), "~/smain/%s", filename);
+    remove(file_path);
+
+    // Request removal from Spdf or Stext if the file is .pdf or .txt
+    if (strstr(filename, ".txt") != NULL)
+    {
+        int stext_sock;
+        connect_to_server("127.0.0.1", STEXT_PORT, &stext_sock);
+        send(stext_sock, filename, strlen(filename), 0);
+        close(stext_sock);
+    }
+    else if (strstr(filename, ".pdf") != NULL)
+    {
+        int spdf_sock;
+        connect_to_server("127.0.0.1", SPDF_PORT, &spdf_sock);
+        send(spdf_sock, filename, strlen(filename), 0);
+        close(spdf_sock);
+    }
+}
+
+void handle_dtar(int client_sock, char *filetype) {
+    char buffer[BUFFER_SIZE];
+    FILE *tar_file;
+    size_t file_size;
+
+    if (strcmp(filetype, ".c") == 0) {
+        // Handle .c files locally on Smain server
+        system("find ~/smain -name '*.c' | tar -cvf cfiles.tar -T -");
+        tar_file = fopen("cfiles.tar", "rb");
+    } else if (strcmp(filetype, ".pdf") == 0) {
+        printf("hello\n");
+        // Handle .pdf files on Spdf server
+        int spdf_sock;
+        connect_to_server(PDF_ADDRESS, SPDF_PORT, &spdf_sock);
+        printf("hello2\n");
+        // Send command to Spdf server to create the tar file
+        send(spdf_sock, "dtar .pdf", strlen("dtar .pdf"), 0);
+        
+        //tar_file = fopen("pdffiles.tar", "wb");
+
+        // Receive the tar file from Spdf server
+        // while ((file_size = recv(spdf_sock, buffer, BUFFER_SIZE, 0)) > 0) {
+        //     fwrite(buffer, 1, file_size, tar_file);
+        //     if (file_size < sizeof(buffer))
+        //     {
+        //         printf("End of file detected\n");
+        //         break; // End of file
+        //     }
+        // }
+
+        //fclose(tar_file);
+        close(spdf_sock);
+
+        // Open the tar file to send it to the client
+        //tar_file = fopen("pdffiles.tar", "rb");
+    } else if (strcmp(filetype, ".txt") == 0) {
+        // Handle .txt files on Stext server
+        int stext_sock;
+        connect_to_server(TEXT_ADDRESS, STEXT_PORT, &stext_sock);
+
+        // Send command to Stext server to create the tar file
+        send(stext_sock, "dtar .txt", strlen("dtar .txt"), 0);
+    
+        close(stext_sock);
+    } else {
+        char *msg = "Unsupported file type\n";
+        send(client_sock, msg, strlen(msg), 0);
+        return;
+    }
+
+    char response[BUFFER_SIZE];
+        snprintf(response, sizeof(response), "File uploaded successfully\n");
+        send(client_sock, response, strlen(response), 0);
+}
+
+
+void handle_display(int client_sock, char *pathname)
+{
+    char command[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
+    FILE *pipe;
+    size_t file_size;
+
+    snprintf(command, sizeof(command), "find ~/smain/%s -type f ( -name '*.c' -o -name '*.pdf' -o -name '*.txt' )", pathname);
+    pipe = popen(command, "r");
+    if (!pipe)
+    {
+        perror("popen failed");
+        return;
+    }
+
+    while ((file_size = fread(buffer, 1, BUFFER_SIZE, pipe)) > 0)
+    {
+        send(client_sock, buffer, file_size, 0);
+    }
+
+    pclose(pipe);
+}
+
+void connect_to_server(const char *ip, int port, int *sock)
+{
+    struct sockaddr_in server_addr;
+
+    if ((*sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, ip, &server_addr.sin_addr) <= 0)
+    {
+        perror("Invalid address");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(*sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+}
---
+++ client24s.c
@@ -0,0 +1,300 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <limits.h>
+
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
+
+#define PORT 8090
+#define BUFFER_SIZE 1024
+
+void send_command(int client_sock, const char *command, const char *arg1, const char *arg2)
+{
+    char buffer[BUFFER_SIZE];
+    snprintf(buffer, sizeof(buffer), "%s %s %s", command, arg1, arg2);
+
+    if (send(client_sock, buffer, strlen(buffer), 0) == -1)
+    {
+        perror("send_command failed");
+    }
+}
+
+void send_file(int client_sock, const char *filename)
+{
+    char buffer[BUFFER_SIZE];
+    int file = open(filename, O_RDONLY);
+    if (file < 0)
+    {
+        perror("open file");
+        return;
+    }
+    // Check the size of the file
+    off_t file_size = lseek(file, 0, SEEK_END);
+    lseek(file, 0, SEEK_SET); // Rewind to the start of the file
+
+    if (file_size == 0)
+    {
+        printf("File is empty: %s\n", filename);
+        send(client_sock, "", 1, 0);
+    }
+    else
+    {
+        ssize_t bytes_read;
+        while ((bytes_read = read(file, buffer, sizeof(buffer))) > 0)
+        {
+            if (send(client_sock, buffer, bytes_read, 0) == -1)
+            {
+                perror("send file");
+                break;
+            }
+        }
+    }
+
+    close(file);
+}
+
+void split_path(const char *full_path, char *folder_name, char *file_name)
+{
+    // Find the last occurrence of the '/' character
+    const char *last_slash = strrchr(full_path, '/');
+
+    // If a slash was found, separate the folder and file names
+    if (last_slash != NULL)
+    {
+        // Copy the folder name (including the slash)
+        size_t folder_len = last_slash - full_path + 1;
+        strncpy(folder_name, full_path, folder_len);
+        folder_name[folder_len] = '\0';
+
+        // Copy the file name
+        strcpy(file_name, last_slash + 1);
+    }
+    else
+    {
+        // If no slash is found, treat the entire path as a file name
+        strcpy(folder_name, "");
+        strcpy(file_name, full_path);
+    }
+}
+
+// Function to generate a unique filename
+void get_unique_filename(const char *folder_path, char *unique_filename)
+{
+    int i = 1;
+    char temp_filename[BUFFER_SIZE];
+    char folder_name[1024];
+    char base_filename[1024];
+
+    // split_path(folder_path, folder_name, base_filename);
+
+    // Combine the folder path and base filename to form the initial full path
+    snprintf(unique_filename, BUFFER_SIZE, "%s", folder_path);
+
+    // Check if file exists in the specified folder and generate a unique filename if necessary
+    while (access(unique_filename, F_OK) != -1)
+    {
+        snprintf(temp_filename, BUFFER_SIZE, "%s%s(%d)", folder_path, base_filename, i++);
+        snprintf(unique_filename, BUFFER_SIZE, "%s", temp_filename);
+    }
+}
+
+int create_directory_recursive(const char *path)
+{
+    char temp[256];
+    char *p = NULL;
+    size_t len;
+
+    snprintf(temp, sizeof(temp), "%s", path);
+    len = strlen(temp);
+
+    if (temp[len - 1] == '/')
+    {
+        temp[len - 1] = '\0';
+    }
+
+    for (p = temp + 1; *p; p++)
+    {
+        if (*p == '/')
+        {
+            *p = '\0';
+            if (mkdir(temp, 0755) != 0 && errno != EEXIST)
+            {
+                perror("mkdir");
+                return -1;
+            }
+            *p = '/';
+        }
+    }
+
+    return 0;
+}
+
+void receive_file(int client_sock, const char *filename)
+{
+    char buffer[BUFFER_SIZE];
+    FILE *file;
+    ssize_t bytes_received;
+    char unique_filename[BUFFER_SIZE];
+    char file_path[BUFFER_SIZE];
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
+    char folder_name[1024];
+    char base_filename[1024];
+    split_path(filename, folder_name, base_filename);
+    snprintf(file_path, sizeof(file_path), "%s/%s", cwd, base_filename);
+
+    get_unique_filename(file_path, unique_filename);
+
+    file = fopen(unique_filename, "wb");
+    if (file == NULL)
+    {
+        perror("Failed to open file for writing");
+        return;
+    }
+
+    printf("Receiving file: %s\n", unique_filename);
+    // Receive the file data in chunks
+    while ((bytes_received = recv(client_sock, buffer, sizeof(buffer), 0)) >= 0)
+    {
+        fwrite(buffer, 1, bytes_received, file);
+        if (bytes_received < sizeof(buffer))
+        {
+            break; // End of file
+        }
+    }
+
+    if (bytes_received < 0)
+    {
+        perror("Failed to receive file");
+    }
+
+    fclose(file);
+    return;
+}
+
+void handle_command(int client_sock, const char *command, const char *arg1, const char *arg2)
+{
+    if (strcmp(command, "ufile") == 0)
+    {
+        send_command(client_sock, command, arg1, arg2);
+        send_file(client_sock, arg1);
+    }
+    else if (strcmp(command, "dfile") == 0)
+    {
+        send_command(client_sock, command, arg1, arg2);
+        receive_file(client_sock, arg1);
+    }
+    else if (strcmp(command, "rmfile") == 0 || strcmp(command, "dtar") == 0 || strcmp(command, "display") == 0)
+    {
+        send_command(client_sock, command, arg1, arg2);
+    }
+    else
+    {
+        printf("Unknown command: %s\n", command);
+    }
+}
+
+int main()
+{
+    int client_sock;
+    struct sockaddr_in server_addr;
+    char command[BUFFER_SIZE], arg1[BUFFER_SIZE], arg2[BUFFER_SIZE];
+    char *token;
+    char input[BUFFER_SIZE];
+
+    // Create socket
+    if ((client_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set up server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0)
+    {
+        perror("Invalid address");
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to server
+    if (connect(client_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {
+        // Get command from user
+        printf("Enter command: ");
+        if (fgets(input, sizeof(input), stdin) == NULL)
+        {
+            perror("fgets failed");
+            continue;
+        }
+
+        // Remove newline character
+        input[strcspn(input, "\n")] = 0;
+
+        // Tokenize the input
+        token = strtok(input, " ");
+        if (token == NULL)
+        {
+            printf("No command entered.\n");
+            continue;
+        }
+
+        strncpy(command, token, sizeof(command));
+        token = strtok(NULL, " ");
+        if (token != NULL)
+        {
+            strncpy(arg1, token, sizeof(arg1));
+            token = strtok(NULL, " ");
+            if (token != NULL)
+            {
+                strncpy(arg2, token, sizeof(arg2));
+            }
+            else
+            {
+                arg2[0] = '\0';
+            }
+        }
+        else
+        {
+            arg1[0] = '\0';
+            arg2[0] = '\0';
+        }
+
+        // Send the command
+        handle_command(client_sock, command, arg1, arg2);
+        // Receive and display response (optional)
+        char response[BUFFER_SIZE];
+        int len = recv(client_sock, response, sizeof(response) - 1, 0);
+        if (len > 0)
+        {
+            response[len] = '\0'; // Null-terminate the received data
+            printf("Server response: %s\n", response);
+        }
+        else if (len == 0)
+        {
+            printf("Server disconnected.\n");
+            break;
+        }
+        else
+        {
+            perror("recv failed");
+        }
+    }
+
+    close(client_sock);
+    return 0;
+}
